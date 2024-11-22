096af53fd3c9c470f16deb7df2905ff347ca2fb5
januevesUMA <olegariojanine29@gmail.com>
2024-11-22 22:16:40 +0100

    Añadido VisualizarCategoria y dashboard a medias
    He añadido VisualizarCategoria.cs para simular el cambio al apartado Categorias en el dashboard. En MainForm he metido 3 botones sin implementar para simular el dashboard.

---
+++ 1eraIteración/CodeJanine/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dataGridViewCategorias = new System.Windows.Forms.DataGridView();
+            this.textBoxSearchby = new System.Windows.Forms.TextBox();
+            this.Añadir = new System.Windows.Forms.Button();
+            this.Editar = new System.Windows.Forms.Button();
+            this.Borrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCategorias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCategorias
+            // 
+            this.dataGridViewCategorias.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewCategorias.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridViewCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCategorias.Location = new System.Drawing.Point(66, 134);
+            this.dataGridViewCategorias.Name = "dataGridViewCategorias";
+            this.dataGridViewCategorias.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewCategorias.Size = new System.Drawing.Size(918, 282);
+            this.dataGridViewCategorias.TabIndex = 0;
+            this.dataGridViewCategorias.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewProductos_CellContentClick);
+            // 
+            // textBoxSearchby
+            // 
+            this.textBoxSearchby.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBoxSearchby.Location = new System.Drawing.Point(777, 108);
+            this.textBoxSearchby.Name = "textBoxSearchby";
+            this.textBoxSearchby.Size = new System.Drawing.Size(207, 20);
+            this.textBoxSearchby.TabIndex = 1;
+            this.textBoxSearchby.Text = "Search by...";
+            this.textBoxSearchby.Click += new System.EventHandler(this.textBoxSearchby_Click);
+            this.textBoxSearchby.TextChanged += new System.EventHandler(this.textBoxSearchby_TextChanged);
+            this.textBoxSearchby.DoubleClick += new System.EventHandler(this.textBoxSearchby_TextChanged);
+            this.textBoxSearchby.Enter += new System.EventHandler(this.textBoxSearchby_Enter);
+            this.textBoxSearchby.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSearchby_KeyDown);
+            // 
+            // Añadir
+            // 
+            this.Añadir.Image = ((System.Drawing.Image)(resources.GetObject("Añadir.Image")));
+            this.Añadir.Location = new System.Drawing.Point(66, 435);
+            this.Añadir.Name = "Añadir";
+            this.Añadir.Size = new System.Drawing.Size(126, 123);
+            this.Añadir.TabIndex = 2;
+            this.Añadir.Text = "button1";
+            this.Añadir.UseVisualStyleBackColor = true;
+            this.Añadir.Click += new System.EventHandler(this.Añadir_Click);
+            // 
+            // Editar
+            // 
+            this.Editar.Image = ((System.Drawing.Image)(resources.GetObject("Editar.Image")));
+            this.Editar.Location = new System.Drawing.Point(217, 435);
+            this.Editar.Name = "Editar";
+            this.Editar.Size = new System.Drawing.Size(126, 123);
+            this.Editar.TabIndex = 3;
+            this.Editar.UseVisualStyleBackColor = true;
+            this.Editar.Click += new System.EventHandler(this.Editar_Click);
+            // 
+            // Borrar
+            // 
+            this.Borrar.Image = ((System.Drawing.Image)(resources.GetObject("Borrar.Image")));
+            this.Borrar.Location = new System.Drawing.Point(367, 435);
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Size = new System.Drawing.Size(126, 123);
+            this.Borrar.TabIndex = 4;
+            this.Borrar.UseVisualStyleBackColor = true;
+            this.Borrar.Click += new System.EventHandler(this.Borrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Dashboard";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 598);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Borrar);
+            this.Controls.Add(this.Editar);
+            this.Controls.Add(this.Añadir);
+            this.Controls.Add(this.textBoxSearchby);
+            this.Controls.Add(this.dataGridViewCategorias);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCategorias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCategorias;
+        private System.Windows.Forms.TextBox textBoxSearchby;
+        private System.Windows.Forms.Button Añadir;
+        private System.Windows.Forms.Button Editar;
+        private System.Windows.Forms.Button Borrar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 1eraIteración/CodeJanine/MainForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadAllCategorias();
+            dataGridViewCategorias.ClearSelection();
+            
+
+        }
+
+        private void textBoxSearchby_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void dataGridViewCategorias_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void textBoxSearchby_Enter(object sender, EventArgs e)
+        {
+            textBoxSearchby.SelectAll();
+        }
+
+        private void textBoxSearchby_Click(object sender, EventArgs e)
+        {
+            textBoxSearchby.SelectAll();
+        }
+
+        private void textBoxSearchby_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) // Detecta si se presionó Enter
+            {
+                string searchText = textBoxSearchby.Text.Trim(); // Texto ingresado
+                if (string.IsNullOrEmpty(searchText))
+                {
+                    // Si no hay texto, mostrar todas las categorias
+                    LoadAllCategorias();
+                }
+
+            }
+        }
+        public void load()
+        {
+            LoadAllCategorias();
+        }
+        private void LoadAllCategorias()
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+
+            }
+
+        private void Añadir_Click(object sender, EventArgs e)
+        {
+            CrearCategoria añadir = new CrearCategoria(this);
+            añadir.ShowDialog();
+        }
+
+        private void Editar_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewCategorias.SelectedRows[0] != null)
+            {
+                var filaSeleccionada = dataGridViewCategorias.SelectedRows[0];
+                try
+                {
+                    Categoria catg = new Categoria()
+                    {
+                        nombre = filaSeleccionada.Cells[0].Value.ToString()
+                    };
+                    EditarCategoria editar = new EditarCategoria(this, catg);
+                    editar.ShowDialog();
+                }
+                catch (NullReferenceException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+
+        private void Borrar_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewCategorias.SelectedRows[0] != null)
+            {
+                var filaSeleccionada = dataGridViewCategorias.SelectedRows[0];
+                try
+                {
+                    Categoria p = new Categoria()
+                    {
+                        nombre = filaSeleccionada.Cells[0].Value.ToString(),
+                    };
+                    BorrarCategoria borrar = new BorrarCategoria(this, p);
+                    borrar.ShowDialog();
+                }
+                catch (NullReferenceException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+        }
+
+        public void cambiarColor(Color color)
+        {
+            this.BackColor = color;
+        }
+        private void dataGridViewProductos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+}
+
+
+}
+
