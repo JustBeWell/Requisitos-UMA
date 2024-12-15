ca1c1c88a6db4a861e74f05c04a4408353e1a007
Fjjorda <francisco.jorda2001@gmail.com>
2024-12-15 01:58:22 +0100

    Creación de codeGeneral
    Tiene todas las implementaciones funcionales hasta ahora. Esta es la carpeta donde trabajremos el resto del proyecto.

---
+++ IteracionFInal/CodeGeneral/RelacionesMain.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class RelacionesMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RelacionesMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Ayuda = new System.Windows.Forms.Button();
+            this.Relacion = new System.Windows.Forms.Button();
+            this.Atributo = new System.Windows.Forms.Button();
+            this.Categoria = new System.Windows.Forms.Button();
+            this.Assets = new System.Windows.Forms.Button();
+            this.Producto = new System.Windows.Forms.Button();
+            this.Dashboard = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Añadir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Cuenta = new WindowsFormsApplication2.RoundButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Ayuda);
+            this.panel1.Controls.Add(this.Relacion);
+            this.panel1.Controls.Add(this.Atributo);
+            this.panel1.Controls.Add(this.Categoria);
+            this.panel1.Controls.Add(this.Assets);
+            this.panel1.Controls.Add(this.Producto);
+            this.panel1.Controls.Add(this.Dashboard);
+            this.panel1.Location = new System.Drawing.Point(121, 109);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1005, 94);
+            this.panel1.TabIndex = 6;
+            // 
+            // Ayuda
+            // 
+            this.Ayuda.Location = new System.Drawing.Point(873, 22);
+            this.Ayuda.Margin = new System.Windows.Forms.Padding(6);
+            this.Ayuda.Name = "Ayuda";
+            this.Ayuda.Size = new System.Drawing.Size(121, 46);
+            this.Ayuda.TabIndex = 7;
+            this.Ayuda.Text = "Help";
+            this.Ayuda.UseVisualStyleBackColor = true;
+            // 
+            // Relacion
+            // 
+            this.Relacion.Location = new System.Drawing.Point(711, 22);
+            this.Relacion.Margin = new System.Windows.Forms.Padding(6);
+            this.Relacion.Name = "Relacion";
+            this.Relacion.Size = new System.Drawing.Size(150, 46);
+            this.Relacion.TabIndex = 11;
+            this.Relacion.Text = "Relationships";
+            this.Relacion.UseVisualStyleBackColor = true;
+            this.Relacion.Click += new System.EventHandler(this.Relacion_Click);
+            // 
+            // Atributo
+            // 
+            this.Atributo.Location = new System.Drawing.Point(568, 22);
+            this.Atributo.Margin = new System.Windows.Forms.Padding(6);
+            this.Atributo.Name = "Atributo";
+            this.Atributo.Size = new System.Drawing.Size(132, 46);
+            this.Atributo.TabIndex = 10;
+            this.Atributo.Text = "Attributes";
+            this.Atributo.UseVisualStyleBackColor = true;
+            this.Atributo.Click += new System.EventHandler(this.Atributo_Click);
+            // 
+            // Categoria
+            // 
+            this.Categoria.Location = new System.Drawing.Point(425, 22);
+            this.Categoria.Margin = new System.Windows.Forms.Padding(6);
+            this.Categoria.Name = "Categoria";
+            this.Categoria.Size = new System.Drawing.Size(132, 46);
+            this.Categoria.TabIndex = 9;
+            this.Categoria.Text = "Categories";
+            this.Categoria.UseVisualStyleBackColor = true;
+            this.Categoria.Click += new System.EventHandler(this.Categoria_Click);
+            // 
+            // Assets
+            // 
+            this.Assets.Location = new System.Drawing.Point(306, 22);
+            this.Assets.Margin = new System.Windows.Forms.Padding(6);
+            this.Assets.Name = "Assets";
+            this.Assets.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Assets.Size = new System.Drawing.Size(108, 46);
+            this.Assets.TabIndex = 8;
+            this.Assets.Text = "Assets";
+            this.Assets.UseVisualStyleBackColor = true;
+            this.Assets.Click += new System.EventHandler(this.Assets_Click);
+            // 
+            // Producto
+            // 
+            this.Producto.Location = new System.Drawing.Point(176, 22);
+            this.Producto.Margin = new System.Windows.Forms.Padding(6);
+            this.Producto.Name = "Producto";
+            this.Producto.Size = new System.Drawing.Size(119, 46);
+            this.Producto.TabIndex = 7;
+            this.Producto.Text = "Products";
+            this.Producto.UseVisualStyleBackColor = true;
+            this.Producto.Click += new System.EventHandler(this.Producto_Click);
+            // 
+            // Dashboard
+            // 
+            this.Dashboard.Location = new System.Drawing.Point(26, 22);
+            this.Dashboard.Margin = new System.Windows.Forms.Padding(6);
+            this.Dashboard.Name = "Dashboard";
+            this.Dashboard.Size = new System.Drawing.Size(139, 46);
+            this.Dashboard.TabIndex = 6;
+            this.Dashboard.Text = "Dashboard";
+            this.Dashboard.UseVisualStyleBackColor = true;
+            this.Dashboard.Click += new System.EventHandler(this.Dashboard_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 256);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(775, 93);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "List of Relationships";
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(198, 688);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 138);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Añadir
+            // 
+            this.Añadir.Location = new System.Drawing.Point(420, 688);
+            this.Añadir.Margin = new System.Windows.Forms.Padding(6);
+            this.Añadir.Name = "Añadir";
+            this.Añadir.Size = new System.Drawing.Size(1164, 138);
+            this.Añadir.TabIndex = 10;
+            this.Añadir.Text = "Add a new relation";
+            this.Añadir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Añadir.UseVisualStyleBackColor = true;
+            this.Añadir.Click += new System.EventHandler(this.Añadir_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(198, 400);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1386, 277);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Cuenta
+            // 
+            this.Cuenta.Image = ((System.Drawing.Image)(resources.GetObject("Cuenta.Image")));
+            this.Cuenta.Location = new System.Drawing.Point(1750, 40);
+            this.Cuenta.Margin = new System.Windows.Forms.Padding(4);
+            this.Cuenta.Name = "Cuenta";
+            this.Cuenta.Size = new System.Drawing.Size(109, 106);
+            this.Cuenta.TabIndex = 29;
+            this.Cuenta.UseVisualStyleBackColor = true;
+            this.Cuenta.Click += new System.EventHandler(this.Cuenta_Click);
+            // 
+            // RelacionesMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1994, 847);
+            this.Controls.Add(this.Cuenta);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Añadir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "RelacionesMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "MiniPIM";
+            this.Load += new System.EventHandler(this.RelacionesMain_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Ayuda;
+        private System.Windows.Forms.Button Relacion;
+        private System.Windows.Forms.Button Atributo;
+        private System.Windows.Forms.Button Categoria;
+        private System.Windows.Forms.Button Assets;
+        private System.Windows.Forms.Button Producto;
+        private System.Windows.Forms.Button Dashboard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Añadir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private RoundButton Cuenta;
+    }
+}
